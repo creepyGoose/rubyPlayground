f4c4c08483ad53f818e2ae7adb21bd66fce2a25c
Lucas Eid <lucaseid@protonmail.com>
2021-09-28 17:04:33 -0300

    final

--- main.rb
+++ main.rb
@@ -1,30 +1,42 @@
-users = Hash.new
+$users = {}
+ 
+class RegistroCadastro
+ def add
+   puts "Digite seu username"
+   k = gets.chomp
+   puts "Digite sua senha"
+   v = gets.chomp    
+   $users.store(k, v)
+   home()  
+ end
 
-def add(users) 
-  puts "Digite seu username"
-  k = gets.chomp
-  puts "Digite sua senha"
-  v = gets.chomp 
-  users[:k] = v 
-  puts users
-  home()  
-end
-
-def login 
+ def login
     x = true  
     while x do
      puts "Digite seu username"
      k = gets.chomp
      puts "Digite sua senha"
-     v = gets.chomp
-     if (users.has_key?!(k) and users.has_value?!(v))
-       puts 'Falha ao autenticar'    
-     elsif (users.has_key?(k) and users.has_value?(v))
+     v = gets.chomp    
+     if ($users.has_key?(k) and $users.has_value?(v))
        puts 'Usuário Autenticado'
        x = false
+     else 
+      puts 'FALHA AO AUTENTICAR, TENTE NOVAMENTE'
+      puts
      end
     end
    end
+  end
+
+def list
+  47.times { print '-' }
+  puts 
+  puts 'USUÁRIOS CADASTRADOS'
+  puts $users.keys
+  47.times { print '-' }
+  puts
+  home()
+end
 
 def home()
  puts "Bem-vindo ao Autenticador"
@@ -33,12 +45,15 @@ def home()
  puts '1 - Login | 2 - Registrar | 3 - Listar Usuários'
  r = gets.chomp.to_i
  if (r == 2)   
-  add(users)
- elsif r == 1
-   login()
+  RegistroCadastro.add
+ elsif (r == 1)
+   RegistroCadastro.login
+   elsif (r == 3)
+    list()
  end
 end
 
+RegistroCadastro = RegistroCadastro.new
 home()
      
 
